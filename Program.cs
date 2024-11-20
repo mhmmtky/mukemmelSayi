9f97641724ee9cedbb76281490b1f6a50d07a110
mhmmtky <mhmmtky78@gmail.com>
2024-11-20 23:39:23 +0300

    mukemmelSayi

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mükemmel_sayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayıyı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0) {
+                    toplam = toplam + i;
+                    
+                }
+            }
+            if (toplam == sayi)
+                Console.WriteLine(sayi + " sayısı mükemmel sayıdır.");
+            else
+                Console.WriteLine(sayi + " sayısı mükemmel sayı değildir.");
+            Console.ReadKey();
+        }
+    }
+}
